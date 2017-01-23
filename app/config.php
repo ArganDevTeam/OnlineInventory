b93bb6bbd68fb4ee8c4f886676af801e738c4de7
Marcos Stival Tome <markcosta15@hotmail.com>
2017-01-23 13:01:39 +0100

    Update config.php

--- app/config.php
+++ app/config.php
@@ -1,12 +1,4 @@
 <?php
-/*
-define('DB_HOST', 'localhost');
-define('DB_USER', 'mrcotcbe_admin');
-define('DB_PASS', 'MzTbD!}gNqHw');
-define('DB_NAME', 'mrcotcbe_multivendor');
-
-
-*/
 
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
